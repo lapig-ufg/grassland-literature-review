89210261eafc147cab93dfb5c2733c0b2fa819ca
Jairo Matos da Rocha <devjairomr@gmail.com>
2024-06-04 13:45:26 -0300

    integração

--- src/app/layout/app.topbar.component.ts
+++ src/app/layout/app.topbar.component.ts
@@ -1,15 +1,28 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component, ElementRef, ViewChild, OnInit } from '@angular/core';
 import { LayoutService } from 'src/app/layout/service/app.layout.service';
+import { StatusSourceService } from '../service/status-source';
+import { StatusSource } from '../shared/interface/source';
+import { TableServices } from '../service/table.service';
 
 @Component({
     selector: 'app-topbar',
     templateUrl: './app.topbar.component.html'
 })
-export class AppTopbarComponent {
+export class AppTopbarComponent implements OnInit {
+    statisSource!: StatusSource;
 
     @ViewChild('menubutton') menuButton!: ElementRef;
 
-    constructor(public layoutService: LayoutService) { }
+    constructor(public layoutService: LayoutService,
+        private statusSource: StatusSourceService, 
+        private tableService: TableServices
+    ) { }
+
+    ngOnInit() {
+        this.statusSource.statusSource$.subscribe(dados => {
+          this.statisSource = dados;
+        });
+    }
 
     onMenuButtonClick() {
         this.layoutService.onMenuToggle();
@@ -21,5 +34,10 @@ export class AppTopbarComponent {
     onConfigButtonClick() {
         this.layoutService.showConfigSidebar();
     }
+
+    public applyFilter(query: string): void {
+        console.log("CLiquei no botão")
+        this.tableService.applyFilter(query);
+      }
     
 }
